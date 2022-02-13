d4f779ccdfaa03a081c524d6b8bcdc0f1391781b
HackMort <jonathan.juarezwp@gmail.com>
2022-02-13 00:42:05 -0600

    A little JS functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,22 @@
-console.log('Hello World');
+const smoothScrollLinks = document.querySelectorAll('.smooth-link');
+smoothScrollLinks.forEach(link => {
+  link.addEventListener('click', function (event) {
+    event.preventDefault();
+    const sectionID = link.getAttribute('href');
+    document.querySelector('' + sectionID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  });
+});
+
+const ghLinks = document.querySelectorAll('.gh-link');
+ghLinks.forEach(link => {
+  link.addEventListener('click', function (event) {
+    event.preventDefault();
+    // confirm before opening new tab
+    if (confirm('Would you like to visit the Repository on Github?')) {
+      window.open(link.getAttribute('href'));
+    }
+  });
+});
